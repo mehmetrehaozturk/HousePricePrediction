2ceb135225d4868c77accfecdb089d1eede1fe92
Reha ÖZTÜRK <147929988+mehmetrehaozturk@users.noreply.github.com>
2024-02-02 01:10:57 +0300

    House Pricing Prediction

---
+++ prediction.py
@@ -0,0 +1,222 @@
+################################################################
+# Ev Fiyat Tahmin Modeli
+################################################################
+
+
+# Görev
+# Elimizdeki veri seti üzerinden minimum hata ile ev fiyatlarını tahmin eden bir makine öğrenmesi modeli geliştiriniz ve kaggle yarışmasına tahminlerinizi yükleyiniz.
+# https://www.kaggle.com/competitions/house-prices-advanced-regression-techniques/overview/evaluation
+
+
+# 1. GEREKLILIKLER
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+from catboost import CatBoostRegressor
+from lightgbm import LGBMRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.exceptions import ConvergenceWarning
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split, cross_val_score,GridSearchCV
+
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter("ignore", category=ConvergenceWarning)
+
+
+pd.set_option('display.max_columns', None)
+#pd.set_option('display.max_rows', None)
+pd.set_option('display.width', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+
+######################################
+# GÖREV 1 : Veri setine EDA işlemlerini uygulayınız.
+######################################
+
+# 1. Genel Resim
+# 2. Kategorik Değişken Analizi (Analysis of Categorical Variables)
+# 3. Sayısal Değişken Analizi (Analysis of Numerical Variables)
+# 4. Hedef Değişken Analizi (Analysis of Target Variable)
+# 5. Korelasyon Analizi (Analysis of Correlation)
+
+################################################################
+# Adım 1: Train ve Test veri setlerini okutup birleştiriniz. Birleştirdiğiniz veri üzerinden ilerleyiniz.
+################################################################
+
+# train ve test setlerinin bir araya getirilmesi.
+train = pd.read_csv("data/test.csv")
+test = pd.read_csv("data/train.csv")
+df = pd.concat([train,test],  ignore_index=False).reset_index()
+
+df = df.drop("index", axis=1)
+
+
+######################################
+# 1. Genel Resim
+######################################
+
+def check_df(dataframe):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(3))
+    print("##################### Tail #####################")
+    print(dataframe.tail(3))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe)
+
+check_df(df)
+
+
+
+##################################
+# NUMERİK VE KATEGORİK DEĞİŞKENLERİN YAKALANMASI
+##################################
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+    grab_col_names for given dataframe
+
+    :param dataframe:
+    :param cat_th:
+    :param car_th:
+    :return:
+    """
+
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+
+    # cat_cols + num_cols + cat_but_car = değişken sayısı.
+    # num_but_cat cat_cols'un içerisinde zaten.
+    # dolayısıyla tüm şu 3 liste ile tüm değişkenler seçilmiş olacaktır: cat_cols + num_cols + cat_but_car
+    # num_but_cat sadece raporlama için verilmiştir.
+
+    return cat_cols, cat_but_car, num_cols
+
+cat_cols, cat_but_car, num_cols = grab_col_names(df)
+
+
+######################################
+# 2. Kategorik Değişken Analizi (Analysis of Categorical Variables)
+######################################
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show()
+
+
+for col in cat_cols:
+    cat_summary(df, col)
+
+
+
+######################################
+# 3. Sayısal Değişken Analizi (Analysis of Numerical Variables)
+######################################
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=50)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show()
+
+    print("#####################################")
+
+
+for col in num_cols:
+    num_summary(df, col, True)
+
+
+
+######################################
+# 4. Hedef Değişken Analizi (Analysis of Target Variable)
+######################################
+
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean()}), end="\n\n\n")
+
+
+for col in cat_cols:
+    target_summary_with_cat(df,"SalePrice",col)
+
+
+# Bağımlı değişkenin incelenmesi
+df["SalePrice"].hist(bins=100)
+plt.show()
+
+# Bağımlı değişkenin logaritmasının incelenmesi
+np.log1p(df['SalePrice']).hist(bins=50)
+plt.show()
+
+######################################
+# 5. Korelasyon Analizi (Analysis of Correlation)
+######################################
+
+corr = df[num_cols].corr()
+corr
+
+# Korelasyonların gösterilmesi
+sns.set(rc={'figure.figsize': (12, 12)})
+sns.heatmap(corr, cmap="RdBu")
+plt.show()
+
+
+
+def high_correlated_cols(dataframe, plot=False, corr_th=0.70):
+    corr = dataframe.corr()
+    cor_matrix = corr.abs()
+    upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(np.bool))
+    drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col] > corr_th)]
+    if plot:
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        sns.set(rc={'figure.figsize': (15, 15)})
+        sns.heatmap(corr, cmap="RdBu")
+        plt.show()
+    return drop_list
+
+high_correlated_cols(df, plot=False)
+
+
+
